f1420c8660fd32c8b6dbdb6114ce39bcc5308b87
Jiashu Yang <naxx09@gmail.com>
2017-03-28 20:28:47 -0400

    init

---
+++ HalfTheString/HalfTheString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HalfTheString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string which its length is an even number: ");
+            string str = Console.ReadLine();
+
+            if (str.Length % 2 == 0)
+            {
+                string strHalf = str.Remove(str.Length / 2);
+                Console.WriteLine(strHalf);
+            }
+            else
+            {
+                Console.WriteLine("The string is invalid.");
+            }
+
+            Console.Write("\nEnter any key to continue ...");
+            Console.ReadKey();
+        }
+    }
+}
